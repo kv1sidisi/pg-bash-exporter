537c7339c84d73726c34d6aef73a132833ae5d13
kvisidisi <lyapunovvadim5002@gmail.com>
2025-07-15 00:43:38 +0300

    feat: add config validation as separate command and on startup

--- cmd/exporter/main.go
+++ cmd/exporter/main.go
@@ -4,13 +4,34 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 	"pg-bash-exporter/internal/config"
 )
+
+var ValidationFlag bool
+
+func init() {
+	flag.BoolVar(&ValidationFlag, "validate-config", false, "Validate the configuration file.")
+}
 
 func main() {
 	flag.Parse()
 
 	configPath := config.GetPath()
+
+	if ValidationFlag {
+		var cfg config.Config
+
+		fmt.Println("Validating configuration file:", configPath)
+
+		if err := config.Load(configPath, &cfg); err != nil {
+			log.Fatalf("configuration is invalid: %v", err)
+		}
+
+		fmt.Println("Configuration is valid.")
+
+		os.Exit(0)
+	}
 
 	var cfg config.Config
 
